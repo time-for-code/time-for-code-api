e519c9ea3b33afda7f3a137c94260ff7598526a9
zlucasftw <ilucasmcpvp@gmail.com>
2025-02-27 21:01:53 -0300

    remove: all frontend mock files

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,17 @@
 import express from "express";
+import cors from "cors";
 
+const corsOptions = {
+    origin: "http://localhost:8080",
+    optionSuccessStatus: 200
+};
 
-// const routes()
+const routes = (app) => {
+
+    app.use(express.json())
+    
+    app.use(cors(corsOptions))
+
+    app.get("/",)
+
+}
